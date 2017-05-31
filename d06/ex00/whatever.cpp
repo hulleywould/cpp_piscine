b1ef8fcaca22bcf5293e8cbc01baeb0fcd674634
Sherwin HULLEY <shulley@e4r19p5.jnb.42.fr>
2017-05-31 15:10:55 +0200

    added makefiles

--- d06/ex00/whatever.cpp
+++ d06/ex00/whatever.cpp
@@ -6,7 +6,7 @@
 /*   By: shulley <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/31 13:29:52 by shulley           #+#    #+#             */
-/*   Updated: 2017/05/31 13:48:18 by shulley          ###   ########.fr       */
+/*   Updated: 2017/05/31 15:08:49 by shulley          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,4 +42,20 @@ T max(T &a, T&b)
 		return b;
 	else
 		return b;
+}
+
+int main( void ) {
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return 0;
 }
